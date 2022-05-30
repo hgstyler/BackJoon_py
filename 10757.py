d4c8654b0318031b2f42f8915cd0abc0da2dae1b
Henry Choi <hgstyle@gmail.com>
2022-05-30 20:27:06 +0900

    05302022 night

---
+++ 10757.py
@@ -0,0 +1,11 @@
+# * ** * ** *** ** * ** *
+# BJ Online Judge Project
+# by hgstyler based on Python
+# * ** * ** *** ** * ** *
+
+# Question 10757, BJ Online Judge
+# https://www.acmicpc.net/problem/10757
+
+a,b = map(int, input().split())
+
+print(a+b)
