73a14f5ca4606c6229a5ab09ed65eba5d81f7221
Henry Choi <hgstyle@gmail.com>
2022-06-12 21:48:15 +0900

    06122022

--- 1759.py
+++ 1759.py
@@ -8,50 +8,29 @@
 
 import sys
 
-def be_encryped(cand):
-    picked = []
+def gen_pw(pws):
+    how_many_vows = 0
+    for i in pws:
+        if i in vowels:
+            how_many_vows += 1
+    if how_many_vows >= 1 and l - how_many_vows >= 2:
+        print(''.join(pws))
+ 
+def pw_checker(v, pws):
+    if len(pws) == l:
+        gen_pw(pws)
+        return
+    if v == c:
+        return
+    pws.append(alpha_list[v])
+    pw_checker(v+1, pws)
+    pws.pop()
+    pw_checker(v+1, pws)
 
-    for i1 in range(0, l):
-        picked.append(cand[i1])
-        for i2 in range(i1 + 1, l + 1):
-            picked.append(cand[i2])
-            for i3 in range(i2 + 1, l + 2):
-                picked.append(cand[i3])
-                for i4 in range(i3 + 1, l + 3):
-                    picked.append(cand[i4])
+input = sys.stdin.readline
 
-    return picked
-
-def alpha_check(cand):
-    sub_cnt = 0
-
-    for ii in range(len(cand)):
-        if cand[ii] == 'a' or cand[ii] == 'e' or cand[ii] == 'i' or cand[ii] == 'o' or cand[ii] == 'u':
-            sub_cnt += 1
-
-    if sub_cnt == 1 or sub_cnt == 2:
-        return True
-    else:
-        return False
-
-l,c = map(int, sys.stdin.readline().split())
-candidates = []
-passwd = []
-
-candidates = list(sys.stdin.readline().split())
-print(candidates)
-
-candidates = [ord(x) for x in candidates]
-print(candidates)
-
-candidates.sort()
-print(candidates)
-
-candidates = [chr(x) for x in candidates]
-print(candidates)
-
-passwd = be_encryped(candidates)
-
-for i in passwd:
-    if alpha_check(i):
-        print(i)
+l, c = map(int, input().split())
+alpha_list = sorted(input().split())
+vowels = set(['a', 'e', 'i', 'o', 'u'])
+ 
+pw_checker(0, [])
