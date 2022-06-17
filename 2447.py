cb190fde4dc3ec997538faf3fc80712de567f412
Henry Choi <hgstyle@gmail.com>
2022-06-17 18:11:00 +0900

    06172022 afternoon

--- 2447.py
+++ 2447.py
@@ -7,6 +7,27 @@
 # https://www.acmicpc.net/problem/2447
 
 import sys
+sys.setrecursionlimit(10 ** 6) # https://fuzzysound.github.io/sys-setrecursionlimit
 input = sys.stdin.readline
 
+def star(l):
+    if l == 3:
+        return ['***','* *','***']
 
+    arr = star(l//3)
+    stars = []
+
+    for i in arr:
+        stars.append(i*3)
+
+    for i in arr:
+        stars.append(i+' '*(l//3)+i)
+
+    for i in arr:
+        stars.append(i*3)
+
+    return stars
+
+n = int(input())
+
+print('\n'.join(star(n)))
