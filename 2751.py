10495da6c694fad5493327006ca82c5a5d376f6d
Henry Choi <hgstyle@gmail.com>
2022-06-27 13:14:00 +0900

    06272022

---
+++ 2751.py
@@ -0,0 +1,21 @@
+# * ** * ** *** ** * ** *
+# BJ Online Judge Project
+# by hgstyler based on Python
+# * ** * ** *** ** * ** *
+
+# Question 2751, BJ Online Judge
+# https://www.acmicpc.net/problem/2751
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+numbers = []
+
+for i in range(n):
+    numbers.append(int(input()))
+
+numbers.sort()
+
+for i in range(len(numbers)):
+    print(numbers[i])
