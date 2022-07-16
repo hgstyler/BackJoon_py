ae5c187e729fa0f81da72af70ab4935b44447def
Henry Choi <hgstyle@gmail.com>
2022-07-16 16:04:49 +0900

    07162022

---
+++ 3053.py
@@ -0,0 +1,18 @@
+# * ** * ** *** ** * ** *
+# BJ Online Judge Project
+# by hgstyler based on Python
+# * ** * ** *** ** * ** *
+
+# Question 3053, BJ Online Judge
+# https://www.acmicpc.net/problem/3053
+
+import sys
+import math
+input = sys.stdin.readline
+
+r = int(input())
+
+print("{:.6f}".format(r*r*math.pi))
+print("{:.6f}".format(r*r*2))
+
+# END
